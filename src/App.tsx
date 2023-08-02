e31c146ceab63d688e2c045217e5d59b4ec82505
Sandeep Suman <sandeep.suman@dunzo.in>
2023-08-02 23:22:49 +0530

    React seekhing

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav>
+          <ul>
+            <li><a href="#hero">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#contact">Degub</a></li>
+          </ul>
+        </nav> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
